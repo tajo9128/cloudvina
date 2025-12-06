01f8b2d97b17ff24bf8274756bae80870cb371da
tajo9128 <tajo9128@gmail.com>
2025-12-06 21:40:09 +0530

    fix(build): Add all dependencies to pre-install in build script

--- api/render_build.sh
+++ api/render_build.sh
@@ -5,8 +5,8 @@ set -o errexit
 echo "Build Start: Installing dependencies..."
 pip install --upgrade pip
 
-# Pre-install build dependencies
-pip install six numpy setuptools wheel
+# Pre-install build dependencies (including sklearn for ODDT setup)
+pip install six numpy setuptools wheel scikit-learn joblib pandas scipy
 
 # Install ODDT with --no-build-isolation to use the pre-installed 'six'
 echo "Installing ODDT..."
