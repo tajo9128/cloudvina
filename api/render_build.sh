077387d008f8f50062785f2338e98450d6b72d44
tajo9128 <tajo9128@gmail.com>
2025-12-06 21:33:24 +0530

    chore: Add custom render_build.sh for ODDT

---
+++ api/render_build.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+# Exit on error
+set -o errexit
+
+echo "Build Start: Installing dependencies..."
+pip install --upgrade pip
+
+# Pre-install build dependencies
+pip install six numpy setuptools wheel
+
+# Install ODDT with --no-build-isolation to use the pre-installed 'six'
+echo "Installing ODDT..."
+pip install --no-build-isolation oddt>=0.7
+
+# Install remaining requirements
+echo "Installing requirements.txt..."
+pip install -r api/requirements.txt
+
+echo "Build Complete!"
